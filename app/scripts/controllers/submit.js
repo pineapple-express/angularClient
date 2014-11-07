f994aeb993e104efcbf943bde4b6eab714710b8d
Alana Cable <alana.cable@bancvue.com>
2014-11-07 11:59:28 -0600

    actually can create tags now

--- app/scripts/controllers/submit.js
+++ app/scripts/controllers/submit.js
@@ -1,15 +1,18 @@
 angular.module('softKittyApp')
   .controller('SubmitController', function ($scope, $http) {
-  	$scope.master = {};
+  	$scope.master = ""
 
 	$scope.happy = function(text) {
 		console.log("Happy", createLogEntry(text, "happy"));
+		$scope.text = angular.copy($scope.master);
 	} 	
-	$scope.meh = function() {
+	$scope.meh = function(text) {
 		console.log("Meh", createLogEntry(text, "meh"));
+		$scope.text = angular.copy($scope.master);
 	}
-	$scope.sad = function() {
+	$scope.sad = function(text) {
 		console.log("Sad", createLogEntry(text, "sad"));
+		$scope.text = angular.copy($scope.master);
 	}
 
 	function createLogEntry(text, rating) {
@@ -21,13 +24,19 @@ angular.module('softKittyApp')
 	}
 
 	function getTagsFromText(text) {
+		var tags = [];
 		if (text === undefined) {
-			return [""];
+			return tags;
 		}
 		var list = text.split(" ");
-		for (var s : list) {
 
+		for (var i = 0; i < list.length; i++) {
+			var s = list[i];
+			if (s.indexOf("#") == 0) {
+				tags.push(s.substring(1));
+			}
 		}
+		return tags;
 	}
 
  });
