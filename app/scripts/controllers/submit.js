aca4bc8d8fa4548908ee0fdd35b7ebaf1c3bd66f
Alana Cable <alana.cable@bancvue.com>
2014-11-07 13:49:27 -0600

    Actually posting data to node

--- app/scripts/controllers/submit.js
+++ app/scripts/controllers/submit.js
@@ -2,20 +2,17 @@
 
 angular.module('softKittyApp')
   .controller('SubmitController', function ($scope, $http) {
-  	$scope.master = ''
+  	$scope.master = '';
 
 	$scope.happy = function(text) {
-		console.log('Happy', createLogEntry(text, 'happy'));
-		$scope.text = angular.copy($scope.master);
-	} 	
+		submit(createLogEntry(text, 'happy'));
+	};
 	$scope.meh = function(text) {
-		console.log('Meh', createLogEntry(text, 'meh'));
-		$scope.text = angular.copy($scope.master);
-	}
+		submit(createLogEntry(text, 'meh'));
+	};
 	$scope.sad = function(text) {
-		console.log('Sad', createLogEntry(text, 'sad'));
-		$scope.text = angular.copy($scope.master);
-	}
+		submit(createLogEntry(text, 'sad'));
+	};
 
 	function createLogEntry(text, rating) {
 		var logEntry = {};
@@ -34,11 +31,23 @@ angular.module('softKittyApp')
 
 		for (var i = 0; i < list.length; i++) {
 			var s = list[i];
-			if (s.indexOf('#') == 0) {
+			if (s.indexOf('#') === 0) {
 				tags.push(s.substring(1));
 			}
 		}
 		return tags;
+	}
+
+	function submit(logEntry) {
+		$http.post('http://localhost:8080/api/logEntry', angular.toJson(logEntry))
+			.success(function (data, status, headers, config) {
+				$scope.text = angular.copy($scope.master);
+			})
+			.error(function(data, status, headers, config) {
+				console.log('Unable to post', data);
+				console.log('status', status);
+				console.log('headers', headers);
+			});
 	}
 
  });
